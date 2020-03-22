323868621a5130404598c35861a6fb31d0a7576e
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-03-22 18:42:21 +0100

    add useEffects

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,17 +1,19 @@
 import React, {useEffect} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {Container } from '@material-ui/core'
 import queryString from 'query-string'
 
 import {searchMovie} from '../../redux/actions/search'
-
+import {movieResults, isSearchLoading} from '../../redux/selectors'
 export default ({location}) => {
     const dispatch = useDispatch()
+    const movies = useSelector(state => movieResults(state))
+    console.log(movies)
     useEffect(() => {
         const { movieName } = queryString.parse(location.search)
         dispatch(searchMovie({ movieName }))
 
-    })
+    }, [])
     return (
         <Container> RESULTADO </Container>
     )
--- src/redux/reducers/search.js
+++ src/redux/reducers/search.js
@@ -17,22 +17,18 @@ export default function (state = initialState, action) {
                 isLoading: true,
                 movies: null
             };
-        break;
         case SEARCH_MOVIE_COMPLETE:
-            console.log(action)
             return {
                 ...state,
                 isLoading: false,
                 movieResults: get(action, 'results.data')
             };
-        break;
         case SEARCH_MOVIE_ERROR:
             return {
                 ...state,
                 isLoading: false,
                 movies: null
             };
-        break;
         default: 
             return {
             ...state
